8279ff6bc5f3e166da4d9bbd062811682dbdd5fe
Justineo <justice360@gmail.com>
2026-02-27 15:10:18 +0800

    feat: enable vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { inject } from "@vercel/analytics";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "overlayscrollbars/styles/overlayscrollbars.css";
 import "./styles/global.css";
 
+inject();
 createApp(App).mount("#app");
